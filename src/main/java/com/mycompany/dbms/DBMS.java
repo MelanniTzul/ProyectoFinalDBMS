970c893ffb78984697586d453bc4aae5b9b50ece
MelanniTzul <melannitzul@gmail.com>
2023-04-24 01:08:58 -0600

    Implementacion de interfaz

---
+++ src/main/java/com/mycompany/dbms/DBMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.dbms;
+
+import com.mycompany.dbms.frontend.Menu;
+
+
+/**
+ *
+ * @author melanni
+ */
+public class DBMS {
+
+    public static void main(String[] args) {
+        Menu ventana = new Menu();
+        ventana.setVisible(true);
+            
+    }
+}
